51dd6640c711f4d0de3f4f55bbd52182711861bc
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-09 18:42:10 +0000

    enforce max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,20 @@
 class Oystercard
 
   attr_reader :balance
+  MAX_BALANCE = 90
 
+  def initialize(balance=0)
+    @balance = balance
+  end
 
-  def initialize
-    @balance = 0
+  def top_up(amount)
+    fail "Max balance is 90" if exceed?(@balance + amount)
+    @balance += amount
+  end
+
+  private
+
+  def exceed?(balance)
+    true if balance > MAX_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,8 +3,34 @@ require 'oystercard'
 describe Oystercard do
 
   describe 'initialization' do
-    it 'has a balance of zero when first created' do
-      expect(Oystercard.new.balance).to eq(0)
+    it 'is created with a balance of zero by default' do
+      oystercard = Oystercard.new(0)
+      expect(oystercard.balance).to eq(0)
     end
   end
+
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'can be topped up with a specific amount' do
+      oystercard = Oystercard.new(0)
+      oystercard.top_up(20)
+      expect(oystercard.balance).to eq(20)
+    end
+
+    it 'raises an error when the balance exceeds the max limit' do
+      oystercard = Oystercard.new(0)
+      message = "Max balance is 90"
+      expect { oystercard.top_up(91) }.to raise_error message
+    end
+
+    it 'raises an error when the balance exceeds the max limit' do
+      oystercard = Oystercard.new(0)
+      message = "Max balance is 90"
+      expect { oystercard.top_up(91) }.to raise_error message
+      expect(oystercard.balance).to eq (0)
+    end
+
+  end
+
 end
