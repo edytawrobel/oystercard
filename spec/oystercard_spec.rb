979e785aabcbde8a32965659d0facfe8f8932ab5
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-09 15:58:28 +0000

    adds rspec initialize to test for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
 
+  describe 'initialization' do
+    it 'has a balance of zero when first created' do
+      expect(Oystercard.new.balance).to eq(0)
+    end
+  end
 end
