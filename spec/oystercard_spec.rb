54b34fc0668567232656b5873c1fdc3f8f4f34b9
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-10 11:40:49 +0000

    deduct the balance by specific amount

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,7 +12,7 @@ describe Oystercard do
     it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
-      expect{ oystercard.top_up 10 }.to change{ subject.balance }.by 10
+      expect{ oystercard.top_up 10 }.to change{ oystercard.balance }.by 10
     end
 
     it 'can be topped up with a specific amount' do
@@ -25,6 +25,15 @@ describe Oystercard do
       message = "You have exceeded #{max_balance}!"
       expect { oystercard.top_up(91) }.to raise_error message
       expect(oystercard.balance).to eq (0)
+    end
+
+    describe '#deduct' do
+      it { is_expected.to respond_to(:deduct).with(1).argument }
+
+      it 'deducts the balance by specific amount' do
+          oystercard.top_up(20)
+          expect{ oystercard.deduct(5) }.to change{ oystercard.balance}.by -5
+      end
     end
 
   end
